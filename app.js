5af799c22df8d519418a3ad6fd85a8037b226d31
Lucas Alves <43503542+alves1x@users.noreply.github.com>
2024-09-19 20:40:55 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+function pesquisar() {
+let section = document.getElementById("resultados-pesquisa");
+
+let campoPesquisa = document.getElementById("campo-pesquisa").value;
+if (campoPesquisa == "") {
+    section.innerHTML = "<p> Esse jogador não foi encontrado!"
+    return
+}
+
+campoPesquisa = campoPesquisa.toLowerCase()
+let resultados = "";
+let titulo = "";
+let descricao = "";
+let tags = "";
+
+for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.titulo.toLowerCase()
+    tags = dado.tags.toLowerCase()
+
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        resultados += `
+        <div class="item-resultado">
+            <h2>
+                <a href="#" target="_blank">${dado.titulo}</a>
+            </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Mais Informações</a>
+                </div>
+    `;
+    }
+}
+
+if (!resultados) {
+    resultados = "<p>Esse jogador não foi encontrado!</p>"
+}
+
+section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,147 @@
+let dados = [
+    {
+        titulo: "Hugo Souza",
+        descricao: "Pos: Goleiro",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Hugo+Souza",
+        tags: " gol brasil brasileiro "
+    },
+    {
+        titulo: "Yuri Alberto",
+        descricao: "Pos: Atacante",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Yuri+Alberto",
+        tags: "yure artilheiro centroavante ataque brasil brasileiro"
+    },
+    {
+        titulo: "Rodrigo Garro",
+        descricao: "Pos: Meia",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Rodrigo+Garro",
+        tags: "meio campo meia central campista argentino argentina " 
+    },
+    {
+        titulo: "Memphis Depay",
+        descricao: "Pos: Atacante",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Memphis",
+        tags: "holandes holandês holanda centroavante"
+    },
+    {
+        titulo: "Igor Coronado",
+        descricao: "Pos: Meia",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Igor+Coronado",
+        tags: "meio campista central centro"
+    },
+    {
+        titulo: "Fagner",
+        descricao: "Pos: Lateral-direito",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Fagner",
+        tags: "fagner lateral direito capitão corinthians"
+      
+    },
+    {
+        titulo: "Maycon",
+        descricao: "Pos: Volante",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Maycon",
+        tags: "maycon volante corinthians forte marcação"
+    },      
+    {
+        titulo: "André Ramalho",
+        descricao: "Pos: Volante",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/André+Ramalho",
+        tags: "andre ramalho volante corinthians experiente"
+    },
+    {
+        titulo: "Cacá",
+        descricao: "Pos: Zagueiro",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Cacá",
+        tags: "caca zagueiro corinthians jovem promissor"
+    },
+    {
+        titulo: "Félix Torres",
+        descricao: "Pos: Zagueiro",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Félix+Torres",
+        tags: "felix torres zagueiro equatoriano equador forte"
+    },
+    {
+        titulo: "Matheus Bidu",
+        descricao: "Pos: Lateral-esquerdo",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Matheus+Bidu",
+        tags: "matheus bidu lateral esquerdo corinthians"
+    },
+    {
+        titulo: "Matheuzinho",
+        descricao: "Pos: Lateral-direito",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Matheuzinho",
+        tags: "matheuzinho lateral direito corinthians jovem"
+    },
+    {
+        titulo: "Breno Bidon",
+        descricao: "Pos: Volante",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Breno+Bidon",
+        tags: "breno bidon volante meia corinthians base"
+    },
+    {
+        titulo: "Charles",
+        descricao: "Pos: Volante",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Charles",
+        tags: "charles volante meia corinthians "
+    },
+    {
+        titulo: "José Martínez",
+        descricao: "Pos: Volante",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/José+Martínez",
+        tags: "jose martinez volante venezuela venezuelano"
+    },
+    {
+        titulo: "André Carrillo",
+        descricao: "Pos: Atacante",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/André+Carrillo",
+        tags: "andre carrillo atacante ponta peruano habilidoso"
+    },
+    {
+        titulo: "Alex Santana",
+        descricao: "Pos: Meia",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Alex+Santana",
+        tags: "alex santana meia corinthians"
+    },
+    {
+        titulo: "Ryan",
+        descricao: "Pos: Volante",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Ryan",
+        tags: " ryan volante corinthians jovem"
+    },
+    {
+        titulo: "Raniele",
+        descricao: "Pos: Volante",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Raniele",
+        tags: "raniele volante corinthians base"
+    },
+    {
+        titulo: "Pedro Henrique",
+        descricao: "Pos: Atacante",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Pedro+Henrique",
+        tags: "pedro henrique atacante ponta corinthians"
+    },
+    {
+        titulo: "Pedro Raul",
+        descricao: "Pos: Atacante",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Pedro+Raul",
+        tags: "pedro raul atacante corinthians centroavante"
+    },
+    {
+        titulo: "Romero",
+        descricao: "Pos: Atacante",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Romero",
+        tags: "romero atacante corinthians paraguai paraguaio ponta angel"
+    },
+    {
+        titulo: "Héctor Hernández",
+        descricao: "Pos: Atacante",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/H%C3%A9ctor+Hern%C3%A1ndez",
+        tags: "hector hernandez atacante espanhol espanha"
+    },
+    {
+        titulo: "Talles Magno",
+        descricao: "Pos: Atacante",
+        link: "https://www.corinthians.com.br/futebol/profissional/elenco/Talles+Magno",
+        tags: " ponta atacante jovem promissor"
+    }
+];
